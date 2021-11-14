9ce6acf0cc06d2ee743d22286ec373382ffe4a72
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-13 22:10:42 -0500

    Added enable/disable methods, formLayout, moved buttons to private fields

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -6,6 +6,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -15,109 +16,155 @@ import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 
+import java.util.Locale;
+
 /**
  * @author ZipCodeWilmington
  */
 public class CashMachineApp extends Application {
 
     private TextField idField = new TextField();
-    private TextField depositField = new TextField();
-    private TextField withdrawField = new TextField();
-    private Text sceneTitle = new Text("Welcome to ZipCodeBank's ATM.");
+    private TextField depositWithdrawField = new TextField();
+    private Text welcomeTitle = new Text("Welcome to ZipCodeBank's ATM.");
+    private TextField loginInfo = new TextField();
+    private TextField balanceInfo = new TextField();
+    private TextField emailInfo = new TextField();
+
     private CashMachine cashMachine = new CashMachine(new Bank());
-    private Alert loginWarning = new Alert(Alert.AlertType.WARNING);
-    private TextArea idTextArea = new TextArea();
+
+    private Button btnLogin = new Button("Login");
+    private Button btnDeposit = new Button("Deposit");
+    private Button btnWithdraw = new Button("Withdraw");
+    private Button btnExit = new Button("Logout");
 
     private GridPane grid = new GridPane();
 
     private Parent createContent() {
         VBox vbox = new VBox(10);
-        vbox.setPrefSize(600, 300);
-        idField.setPromptText("Enter ID number.");
+        vbox.setPrefSize(500, 300);
 
         TextArea areaInfo = new TextArea();
 
-        Text idText = new Text();
-        idTextArea.setPrefHeight(0);
-        //FlowPane idPane = new FlowPane(20, 0, idText, idTextArea);
+        //Button actions for Login, Deposit, Withdraw, Logout
+        enableDisableButtons("on");
+        btnLogin.setOnAction(e -> {
+            loginWarning(idField);
 
-        //GridPane for Form layout
+            int id = Integer.parseInt(idField.getText());
+            cashMachine.login(id);
+
+            loginInfo.setText(cashMachine.toString());
+            enableDisableButtons("off");
+        });
+
+        btnDeposit.setOnAction(e -> {
+            Float amount = Float.parseFloat(depositWithdrawField.getText());
+            cashMachine.deposit(amount);
+
+            //areaInfo.setText(cashMachine.toString());
+        });
+
+        btnWithdraw.setOnAction(e -> {
+            Float amount = Float.parseFloat(depositWithdrawField.getText());
+            cashMachine.withdraw(amount);
+
+            //areaInfo.setText(cashMachine.toString());
+        });
+
+        btnExit.setOnAction(e -> {
+            cashMachine.exit();
+
+            areaInfo.setText(cashMachine.toString());
+            enableDisableButtons("on");
+        });
+
+        //Set form layout
+        formLayout();
+
+        //FlowPanes - id, deposit, withdraw
+        FlowPane loginPane = new FlowPane(10, 0, idField, btnLogin);
+        loginPane.setAlignment(Pos.CENTER);
+
+        FlowPane depositAndWithdrawalPane = new FlowPane(10, 0, depositWithdrawField, btnDeposit, btnWithdraw);
+
+        //Formatting
+        idField.setPromptText("Enter ID number.");
+        depositWithdrawField.setPromptText("Enter amount to deposit or withdraw.");
+        welcomeTitle.setFont(Font.font("Helvetica", FontWeight.MEDIUM, 15));
+
+        vbox.setAlignment(Pos.CENTER);
+        VBox.setMargin(welcomeTitle, new Insets(5, 20, 0, 20));
+        VBox.setMargin(depositAndWithdrawalPane, new Insets(0, 20, 0, 20));
+        vbox.getChildren().addAll(welcomeTitle, loginPane, depositAndWithdrawalPane, grid, btnExit);
+
+        return vbox;
+    }
+
+    private void loginWarning(TextField input) {
+        Alert loginWarning = new Alert(Alert.AlertType.WARNING);
+
+        if (input.getText().equals("")) {
+            loginWarning.setTitle("Login Warning");
+            loginWarning.setHeaderText("You have not entered a login ID."
+                    + '\n' + "Please try again.");
+            loginWarning.showAndWait();
+
+        } else if (input.getText().equals("")) {
+            loginWarning.setTitle("Login Warning");
+            loginWarning.setHeaderText("You have entered an invalid login ID."
+                    + '\n' + "Please try again.");
+            loginWarning.showAndWait();
+        }
+    }
+
+    private void enableDisableButtons(String onOff) {
+        if (onOff.toLowerCase() == "on") {
+            btnDeposit.setDisable(true);
+            btnWithdraw.setDisable(true);
+            btnExit.setDisable(true);
+            btnLogin.setDisable(false);
+        } else {
+            btnDeposit.setDisable(false);
+            btnWithdraw.setDisable(false);
+            btnExit.setDisable(false);
+            btnLogin.setDisable(true);
+        }
+    }
+
+    private void formLayout() {
         grid.setAlignment(Pos.CENTER_LEFT);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(20, 20, 20, 20));
 
-        Label loginID = new Label("Login ID: ");
-        Label balance = new Label("Balance: ");
-        grid.add(loginID, 0, 1);
-        grid.add(balance, 0, 2);
+        //labels to indicate corresponding data fields
+        Label loginIDLabel = new Label("Login ID: ");
+        Label emailLabel = new Label("Email: ");
+        Label balanceLabel = new Label("Balance: ");
 
+        //grid layout
+        grid.add(loginIDLabel, 0, 1);
+        grid.add(emailLabel, 0, 2);
+        grid.add(balanceLabel, 0, 3);
 
-        //Button Creation - Login, Deposit, Withdraw, Exit
-        Button btnLogin = new Button("Login");
-        btnLogin.setOnAction(e -> {
-            //Warning
-            if (idField.getText().equals("")) {
-                loginWarning.showAndWait();
-            }
-            int id = Integer.parseInt(idField.getText());
-            cashMachine.login(id);
+        loginInfo.setDisable(true);
+        loginInfo.setOpacity(0.75);
 
-            areaInfo.setText(cashMachine.toString());
-        });
+        emailInfo.setDisable(true);
+        emailInfo.setOpacity(0.75);
 
-        Button btnDeposit = new Button("Deposit");
-        btnDeposit.setOnAction(e -> {
-            Float amount = Float.parseFloat(depositField.getText());
-            cashMachine.deposit(amount);
+        balanceInfo.setDisable(true);
+        balanceInfo.setOpacity(0.5);
 
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnWithdraw = new Button("Withdraw");
-        btnWithdraw.setOnAction(e -> {
-            Float amount = Float.parseFloat(withdrawField.getText());
-            cashMachine.withdraw(amount);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnExit = new Button("Logout");
-        btnExit.setOnAction(e -> {
-            cashMachine.exit();
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        //Disable Buttons
-        btnDeposit.setDisable(true);
-        btnWithdraw.setDisable(true);
-
-        //FlowPanes - id, deposit, withdraw
-        FlowPane flowpane = new FlowPane();
-
-        FlowPane idFlowPane = new FlowPane();
-        idFlowPane.getChildren().add(idField);
-        idFlowPane.getChildren().add(btnLogin);
-        idFlowPane.setHgap(10);
-
-        flowpane.getChildren().add(btnDeposit);
-        flowpane.getChildren().add(btnWithdraw);
-        flowpane.getChildren().add(btnExit);
-
-
-        //VBox.setMargin(idPane, new Insets(0, 20, 0, 20));
-        vbox.getChildren().addAll(idFlowPane, depositField, withdrawField, flowpane, grid);
-
-        return vbox;
+        grid.add(loginInfo, 1, 1);
+        grid.add(emailInfo, 1, 2);
+        grid.add(balanceInfo, 1, 3);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(createContent()));
-        sceneTitle.setFont(Font.font("Helvetica", FontWeight.MEDIUM, 15));
         stage.setTitle("ZipCodeBank"); //sets the title of the scene
-
         stage.show();
     }
 
